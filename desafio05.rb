d2fbfeff0336f48f2d8b5bd9142b0f4532284044
Anaê Soares <50211193+anaesross@users.noreply.github.com>
2022-06-12 17:18:08 -0300

    Adicionado versão prof nos desafios

--- desafio05.rb
+++ desafio05.rb
@@ -16,3 +16,19 @@ def valida_cpf
 end
 
 valida_cpf
+
+#############Solução professor #############
+def check_cpf(cpf_number)
+ if CPF.valid?(cpf_number)
+   return "O cpf informado é valido"
+ else
+   return "O cpf informado é invalido"
+ end
+end
+ 
+print 'Digite seu cpf: '
+cpf_number = gets.chomp.to_i
+ 
+result = check_cpf(cpf_number)
+ 
+puts result
